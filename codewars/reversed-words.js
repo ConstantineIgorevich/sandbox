8b0f09a3114a25585f413f857ca8a38e3a9fc073
Davydovich <kostiarabota@gmail.com>
2024-09-23 11:09:35 +0300

    feat: add Reversed Words solution

---
+++ codewars/reversed-words.js
@@ -0,0 +1,11 @@
+// description:
+
+// Complete the solution so that it reverses all of the words within the string passed in.
+
+// Words are separated by exactly one space and there are no leading or trailing spaces.
+
+// solution:
+
+function reverseWords(str){
+    return str.split(' ').reverse().join(' ');
+  }
