52a083fa7701f16243e404c0245bbf8bd103af06
Davydovich <kostiarabota@gmail.com>
2024-09-30 09:25:10 +0300

    feat: add No oddities here solution

---
+++ codewars/no-odds.js
@@ -0,0 +1,9 @@
+// description:
+
+// Write a small function that returns the values of an array that are not odd.
+
+// solution:
+
+function noOdds( values ){
+    return values.filter(el => !(el % 2));
+  }
