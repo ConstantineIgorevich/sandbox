411b4f9f61583d30aedf19c235906667613614b6
Davydovich <kostiarabota@gmail.com>
2024-10-11 12:29:11 +0300

    feat: add Return Negative solution

---
+++ codewars/return-negative.js
@@ -0,0 +1,9 @@
+// description:
+
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+// solution:
+
+function makeNegative(num) {
+    return num < 0 ? num : -num;
+  }
