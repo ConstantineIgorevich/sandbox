adb31b679a5d36e71a6fd821586d4a02d125d7e0
Davydovich <kostiarabota@gmail.com>
2024-10-14 11:03:07 +0300

    feat: add Reversed Strings solution

---
+++ codewars/reversed-strings.js
@@ -0,0 +1,9 @@
+// description:
+
+// Complete the solution so that it reverses the string passed into it.
+
+// solution:
+
+function solution(str){
+    return str.split('').reverse().join('');  
+  }
