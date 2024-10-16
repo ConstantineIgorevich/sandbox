2bf26ad746ee7f082203725b85e4111da4635ac0
Davydovich <kostiarabota@gmail.com>
2024-10-16 17:53:19 +0300

    feat: add Opposite number solution

---
+++ codewars/opposite-number.js
@@ -0,0 +1,9 @@
+// description:
+
+// Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+// solution:
+
+function opposite(number) {
+    return(-number);
+  }
