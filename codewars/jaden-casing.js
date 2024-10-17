32104f3e2fceb53df062de5bede95b51a64ce438
Davydovich <kostiarabota@gmail.com>
2024-10-17 11:29:32 +0300

    feat: add Jaden Casing Strings solution

---
+++ codewars/jaden-casing.js
@@ -0,0 +1,12 @@
+// description:
+
+// Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). 
+// Jaden is also known for some of his philosophy that he delivers via Twitter. 
+// When writing on Twitter, he is known for almost always capitalizing every word. 
+// For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below.
+
+// solution:
+
+String.prototype.toJadenCase = function () {
+    return this.split(" ").map(el => el[0].toUpperCase() + el.slice(1)).join(" ");
+  };
