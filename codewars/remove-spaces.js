0f67b74ed48f1eaa695a15cc1b9690db22d86776
Davydovich <kostiarabota@gmail.com>
2024-10-21 13:32:07 +0300

    feat: add Remove String Spaces solution

---
+++ codewars/remove-spaces.js
@@ -0,0 +1,9 @@
+// description:
+
+// Write a function that removes the spaces from the string, then return the resultant string.
+
+// solution:
+
+function noSpace(x) {
+    return x.replaceAll(' ', '');
+  }
