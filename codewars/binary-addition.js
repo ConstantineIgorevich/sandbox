2df40d9d3f87935e1f5986db1c9cbfc3975532bc
Davydovich <kostiarabota@gmail.com>
2024-10-30 12:27:41 +0300

    feat: add Binary Addition solution

---
+++ codewars/binary-addition.js
@@ -0,0 +1,10 @@
+// description:
+
+// Implement a function that adds two numbers together and returns their sum in binary.  
+
+// solution:
+
+function addBinary(a,b){
+    return (a+b).toString(2)
+  }
+
